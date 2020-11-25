200278ca98e143e7a51b055b667c2dde405cc2e2
ColdSnowFlySky <1812210375@qq.com>
2020-11-25 14:16:07 +0800

    feat(onethousand.threehundred.seventy.zero): add question 1370

---
+++ src/onethousand/threehundred/seventy/zero/Solution.java
@@ -0,0 +1,39 @@
+package onethousand.threehundred.seventy.zero;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.print(new Solution().sortString("ggggggg"));
+    }
+    public String sortString(String s) {
+        char[] cs = s.toCharArray();
+        if(cs.length == 0) return s;
+        int[] data = new int[26];
+        List<Integer> list = new ArrayList<>();
+        for(char c : cs) data[c - 'a'] ++;
+        for(int i = 0; i < data.length; i++) if(data[i] > 0) list.add(i);
+        boolean a = true;
+        char[] result = new char[cs.length];
+        int index = 0;
+        while(true) {
+            int i;
+            if(a == true) i = 0;
+            else i = list.size() - 1;
+            while(true) {
+                if(data[list.get(i)] > 0) {
+                    result[index++] = (char)(list.get(i)+'a');
+                    data[list.get(i)]--;
+                }
+                if(a) i++;
+                else i--;
+                if(i < 0 || i >= list.size()) break;
+            }
+            if(index == cs.length) break;
+            if(a) a = false;
+            else a = true;
+        }
+        return String.valueOf(result);
+    }
+}
